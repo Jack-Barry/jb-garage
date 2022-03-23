e2b73aa5e101f75c0c97f5503cf4bbb6a139ed41
Jack Barry <JackBarry@devdad.life>
2022-03-22 21:18:19 -0600

    wip: 🚧  Get a basic Nuxt app running

---
+++ apps/jack-barry-onl/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'jack-barry-onl',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  coverageDirectory: '../../coverage/apps/jack-barry-onl'
+}
