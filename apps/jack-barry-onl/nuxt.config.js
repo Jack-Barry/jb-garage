d489c01c58c396ea8de231c33eefc2ab94e12fb7
Jack Barry <JackBarry@devdad.life>
2022-03-26 16:22:34 -0600

    feat: ✨  Start using Vuetify

--- apps/jack-barry-onl/nuxt.config.js
+++ apps/jack-barry-onl/nuxt.config.js
@@ -1,3 +1,31 @@
 export default {
-  target: 'static'
+  target: 'static',
+  buildModules: [
+    [
+      '@nuxtjs/fontawesome',
+      {
+        icons: { solid: ['faHelmetSafety', 'faMoon', 'faSun'] }
+      }
+    ],
+    [
+      '@nuxtjs/vuetify',
+      {
+        defaultAssets: { icons: 'fa' }
+      }
+    ],
+    '@nuxt/typescript-build'
+  ],
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'A Dad Who Devs',
+    titleTemplate: 'Jack Barry | %s',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  }
 }
