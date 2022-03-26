bfc231786cc737cac89dcd189fc5974a07234b39
Jack Barry <JackBarry@devdad.life>
2022-03-26 16:22:34 -0600

    feat: ✨  Start using Vuetify

---
+++ apps/jack-barry-onl/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app-bar app flat>
+    <v-row>
+      <v-spacer />
+      <btn-dark-mode-toggle />
+    </v-row>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import BtnDarkModeToggle from '@/components/BtnDarkModeToggle.vue'
+
+export default Vue.extend({
+  components: { BtnDarkModeToggle }
+})
+</script>
