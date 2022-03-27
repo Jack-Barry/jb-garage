fd6002a815ef722f783b3d783f8a3f44d4437cc0
Jack Barry <JackBarry@devdad.life>
2022-03-26 18:28:52 -0600

    security: 🔒  Pinning dependencies
    Also fixed some mismatched config for commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,10 @@
+const commitTypes = require('cz-emoji/lib/types.json')
+
 module.exports = {
   extends: ['gitmoji'],
+  rules: {
+    'type-enum': [2, 'always', commitTypes.map((type) => type.name)]
+  },
   parserPreset: {
     parserOpts: {
       headerPattern: /^((?:\w|-)*)(?:\((.*)\))?!?: (.*)$/,
