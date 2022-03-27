ff1777f7227aef3aed7c4bc61fb59c2da8231b41
Jack Barry <JackBarry@devdad.life>
2022-03-26 21:08:01 -0600

    wip: 🚧  Flesh out project structure a bit more

---
+++ libs/vuetify-components/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'vuetify-components',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json'
+    }
+  },
+  coverageDirectory: '../../coverage/libs/vuetify-components'
+}
