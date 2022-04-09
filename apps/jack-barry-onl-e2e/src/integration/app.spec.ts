44081eebe91fed52f5f2a5ba863d6b1da3892627
Jack Barry <Jack@Barry.onl>
2022-04-09 16:36:02 -0600

    prune: 🔥  Clear out bloat from init

--- apps/jack-barry-onl-e2e/src/integration/app.spec.ts
+++ apps/jack-barry-onl-e2e/src/integration/app.spec.ts
@@ -1,13 +1,3 @@
-import { getGreeting } from '../support/app.po'
-
-describe('jack-barry-onl', () => {
+xdescribe('jack-barry-onl', () => {
   beforeEach(() => cy.visit('/'))
-
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword')
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome jack-barry-onl')
-  })
 })
