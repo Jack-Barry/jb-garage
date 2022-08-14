ec35f619a7bd3cc73e4e108666730b9a3e475b13
Jack Barry <Jack@Barry.onl>
2022-08-13 19:26:23 -0600

    wip(jack-barry-onl): 🚧  More Prismic scaffolding

--- apps/jack-barry-onl/pages/_app.tsx
+++ apps/jack-barry-onl/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { AppBar, Box, Toolbar, Typography } from '@mui/material'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import Link from 'next/link'
+
 import ButtonDarkModeToggle from '../components/ButtonDarkModeToggle'
 import AppThemeProvider from '../contexts/app-theme'
+import { linkResolver, repositoryName } from '../prismicio'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import './index.scss'
@@ -11,15 +16,26 @@ config.autoAddCss = false
 export default function App({ Component, pageProps }) {
   return (
     <AppThemeProvider>
-      <Box>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography sx={{ flexGrow: 1 }} />
-            <ButtonDarkModeToggle />
-          </Toolbar>
-        </AppBar>
-        <Component {...pageProps} />
-      </Box>
+      <PrismicProvider
+        linkResolver={linkResolver}
+        internalLinkComponent={({ href, ...props }) => (
+          <Link href={href}>
+            <a {...props} />
+          </Link>
+        )}
+      >
+        <Box>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography sx={{ flexGrow: 1 }} />
+              <ButtonDarkModeToggle />
+            </Toolbar>
+          </AppBar>
+          <PrismicPreview repositoryName={repositoryName}>
+            <Component {...pageProps} />
+          </PrismicPreview>
+        </Box>
+      </PrismicProvider>
     </AppThemeProvider>
   )
 }
---
+++ apps/jack-barry-onl/prismicio.js
@@ -0,0 +1,49 @@
+import * as prismic from '@prismicio/client'
+import * as prismicNext from '@prismicio/next'
+import sm from './sm.json'
+
+/**
+ * @typedef {import('@prismicio/helpers').LinkResolverFunction} LinkResolverFunction
+ */
+
+/**
+ * The project's Prismic repository name.
+ */
+export const repositoryName = prismic.getRepositoryName(sm.apiEndpoint)
+
+/**
+ * The project's Prismic Link Resolver. This function determines the URL for a given Prismic document.
+ *
+ * @type {LinkResolverFunction}
+ */
+// Update the Link Resolver to match your project's route structure
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'home_page':
+      return '/'
+    case 'about_page':
+      return `/about`
+    case 'blog_post':
+      return `/blog/${doc.uid}`
+    default:
+      return null
+  }
+}
+
+/**
+ * Creates a Prismic client for the project's repository. The client is used to
+ * query content from the Prismic API.
+ *
+ * @param config {prismicNext.CreateClientConfig} - Configuration for the Prismic client.
+ */
+export const createClient = (config = {}) => {
+  const client = prismic.createClient(sm.apiEndpoint, config)
+
+  prismicNext.enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req
+  })
+
+  return client
+}
